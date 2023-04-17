56e9027cb518fac27a7b644eb3abb14546c3f8a6
SalZeus <54487998+SalZeus@users.noreply.github.com>
2023-04-17 21:00:01 +0000

    bugfixing

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,45 +1,18 @@
-import React, { useContext, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-import PropTypes from "prop-types";
+import rigoImage from "/workspaces/Jtorres-ContactList/src/img/rigo-baby.jpg"
 
 
 export const Navbar = () => {
-	const [state, setState] = useState({
-		//initialize state here
-	});
-	const {store, actions} = useContext(Context)
-	const[contactName, setName] = useState(store.contacts[props.index]?.contactName||"")
-	const[email, setEmail] = useState(store.contacts[props.index]?.email||"")
-	const[telephone, setTelephone] = useState(store.contacts[props.index]?.telephone||"")
-	const[address, setAddress] = useState(store.contacts[props.index]?.address||"")
-	const[img, setIMG] = useState(store.contacts[props.index]?.img||"")
-
-	function guardar(){
-		let newContact={
-		contactName: contactName, 
-		email:email, 
-		telephone: telephone, 
-		address: address
-		}
-		if (props.index == -1) {
-		// Crear nuevo contacto
-		actions.addContact (newContact)
-		} else if (props.index >= 0) {
-		// Editar contacto
-		actions .editContact (newContact, props. index)
-		} else {
-		// Indice invalido
-		}
-	}
-
+	const {actions}=useContext(Context)
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">4Geeks</span>
 			</Link>
 			<div className="ml-auto">
-					<button onClick={guardar} className="btn btn-primary" data-bs-toggle="modal" data-bs-target={"#addContact-"+props.index}>Add New Contact</button>
+					<button className="btn btn-primary" data-bs-toggle="modal" data-bs-target={"#addContact-"+props.index}>Add New Contact</button>
 			</div>
 		</nav>
 	);
