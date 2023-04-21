bb4a5f863a62fc9179db48db8f299b95afdc7f47
SalZeus <54487998+SalZeus@users.noreply.github.com>
2023-04-21 01:52:02 +0000

    fixed server functionallity by editing flux and apiurl

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -6,29 +6,29 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			contacts: [
-				{
-					"id": "3877",
-					"agenda_slug": "LaReContraGenda",
-					"full_name": "NarutoKun",
-					"email": "yametekudasai@wii.com",
-					"phone": "666666666",
-					"address": "avenda csm carrera de carros 120938",
-					"created_at": "2023-04-18 00:02:50"
-				},
-				{
-					"id": "3878",
-					"agenda_slug": "LaReContraGenda",
-					"full_name": "Chainsaw Man",
-					"email": "aaa@ya.com",
-					"phone": "9898989898",
-					"address": "Somewhere in japan",
-					"created_at": "2023-04-18 00:13:55"
-				}
+				//  {
+				//  	"id": "3877",
+				//  	"agenda_slug": "LaReContraGenda",
+				//  	"full_name": "NarutoKun",
+				//  	"email": "yametekudasai@wii.com",
+				//  	"phone": "666666666",
+				//  	"address": "avenda csm carrera de carros 120938",
+				//  	"created_at": "2023-04-18 00:02:50"
+				//  },
+				//  {
+				//  	"id": "3878",
+				//  	"agenda_slug": "LaReContraGenda",
+				//  	"full_name": "Chainsaw Man",
+				//  	"email": "aaa@ya.com",
+				//  	"phone": "9898989898",
+				//  	"address": "Somewhere in japan",
+				//  	"created_at": "2023-04-18 00:13:55"
+				//  }
 		],
 		},
 		actions: {
 			addContact:async (contact)=>{
-				let response = await fetch("https://assets.breatheco.de/apis/fake/contact/agenda/LaReContraGenda",{
+				let response = await fetch(apiUrl+agendaSlug,{
 					body:JSON.stringify(contact),
 					method:"POST",
 					headers:{
@@ -36,7 +36,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					}
 				})
 				if (!response.ok){
-					console.log(response.status + ": "+ response.statusText)
+					console.log(response.status + ": "+response.statusText)
 					return 
 				}
 				let data=await response.json()
@@ -66,27 +66,28 @@ const getState = ({ getStore, getActions, setStore }) => {
 				setStore({ ...store, contacts: newContacts });
 				console.log(newContacts)
 			},
-			getAgenda:()=>{
-				fetch(apiUrl+"contact/agenda/"+agendaSlug)
-				.then(response=>{
+			getAgenda: () => {
+				fetch (apiUrl + "/agenda/" + agendaSlug)
+				.then(response =>{
+					console.log(response.ok)
 					if(response.ok){
 						return response.json()
-					}
-					else{
-						console.log(response.status+": "+response.statusText)
+					}else{
+						console.log(response.status+":"+response.statusText)
 					}
 				})
 				.then(data=>{
 					console.log(data)
 					setStore({contacts:data})
 				})
-					
 				.catch(error=>{
 					console.error(error)
 				})
-				console.log("iniciada la peticion")
+				console.log("Iniciada la peticion")
 			}
-		}
+			
+		},
+		
 	}
 }
 
